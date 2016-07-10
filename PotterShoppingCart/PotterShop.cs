6f927c5af4ca672a51181a4cca21495bd27c01c8
WizTonE <WizTonE@WIZTONE-PC>
2016-07-10 21:57:37 +0800

    UnitTest initial commit

---
+++ PotterShoppingCart/PotterShop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart
+{
+    public class PotterShop
+    {
+        public int CalculateFee<T>(IEnumerable<T> OrderList)
+        {
+            var Fee = 0;
+            return Fee;
+        }
+    }
+}
---
+++ PotterShoppingCartTests/PotterShopTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass()]
+    public class PotterShopTests
+    {
+        private List<Book> Bootlist { get; set; }
+
+        [TestMethod()]
+        public void CalculateFeeTest()
+        {
+            //arrange
+            var target = new PotterShop();
+
+            //act
+            var actual = target.CalculateFee<Book>(Bootlist);
+
+            //assert
+            Assert.Fail();
+        }
+    }
+
+    public class Book
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+    }
+}
